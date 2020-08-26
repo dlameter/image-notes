b66813694e7533c318a1779484068a1d4401747c
Dominik Lameter <dominik@lameter.com>
2020-08-26 13:08:45 -0500

    Added ability to upload a file and have it shown on in p5

--- image-notes.js
+++ image-notes.js
@@ -1,4 +1,27 @@
+class SceneImage {
+    constructor(image, x, y) {
+        this.img = image;
+        this.x = x;
+        this.y = y;
+    }
+
+    setImg(image) {
+        if (this.img) {
+            this.img.remove();
+        }
+
+        this.img = image;
+    }
+
+    render(p) {
+        if (this.img) {
+            p.image(this.img, this.x, this.y);
+        }
+    }
+}
+
 const s = ( p ) => {
+    let objects = [];
 
     p.setup = function() {
         p.createCanvas(400, 400);
@@ -6,7 +29,39 @@ const s = ( p ) => {
 
     p.draw = function() {
         p.background(220);
+        objects.forEach((object) => {object.render(p)});
+    };
+
+    p.add = function(object) {
+        objects.push(object);
     };
 };
 
 let myp5 = new p5(s);
+
+var bgImage = new SceneImage(null, 0, 0);
+myp5.add(bgImage);
+
+function fileToDateURL(p, file, callback) {
+    let reader = new FileReader();
+
+    reader.addEventListener("load", function () {
+        callback(reader.result);
+    });
+
+    reader.readAsDataURL(file);
+}
+
+function fileChanged(event) {
+    let file = event.target.files[0];
+    fileToDateURL(myp5, file, (fileData) => {
+        bgImage.setImg(myp5.createImg(fileData, ''));
+        bgImage.img.hide();
+    });
+}
+
+window.onload = (event) => {
+    let fileIn = document.getElementById("file-in");
+    fileIn.addEventListener('change', (event) => fileChanged(event));
+}
+
