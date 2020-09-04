06e1a9e14aa81a4f71d6f894015df71e3164b1f1
Dominik Lameter <dominik@lameter.com>
2020-09-04 12:52:04 -0500

    Added data clear and data upload buttons to the interface, as well as a test popup.

--- image-notes.js
+++ image-notes.js
@@ -1,5 +1,6 @@
 var FileSaver = require('./node_modules/file-saver/dist/FileSaver.min.js');
 var map;
+var layerGroup;
 var bgImg;
 
 function setBgImg(url, onload) {
@@ -64,6 +65,52 @@ function saveToJSONToFile(json, name) {
     FileSaver.saveAs(blob, name);
 }
 
+function loadJSONFromFile(event) {
+    let file = event.target.files[0];
+
+    fileToJSONObject(file, (jsonObject) => {
+        newLayerGroup = L.geoJSON(jsonObject);
+        swapLayerGroup(newLayerGroup);
+    });
+}
+
+function fileToJSONObject(file, callback) {
+    let reader = new FileReader();
+
+    reader.addEventListener("load", function () {
+        callback(JSON.parse(reader.result));
+    });
+
+    reader.readAsText(file);
+}
+
+function swapLayerGroup(newLayerGroup) {
+    map.removeLayer(layerGroup);
+    layerGroup.clearLayers();
+    layerGroup = newLayerGroup;
+    addPopupToLayerGroup(layerGroup);
+    layerGroup.addTo(map);
+}
+
+function geomanSetLayerGroup(map, layerGroup){
+    map.on('pm:create', e => {
+        layerGroup.addLayer(e.layer);
+    });
+
+    map.on('pm:cut', e => {
+        layerGroup.removeLayer(e.originalLayer);
+        layerGroup.addLayer(e.layer);
+    });
+
+    map.on('pm:remove', e => {
+        layerGroup.removeLayer(e.layer);
+    });
+}
+
+function addPopupToLayerGroup(layerGroup) {
+    layerGroup.bindPopup('Test');
+}
+
 window.onload = (event) => {
     let fileIn = document.getElementById("file-in");
     fileIn.addEventListener('change', (event) => fileChanged(event));
@@ -75,16 +122,25 @@ window.onload = (event) => {
         zoomDelta: 0.5,
     });
 
-    var layerGroup = L.layerGroup();
+    layerGroup = L.geoJSON();
+    addPopupToLayerGroup(layerGroup);
     layerGroup.addTo(map);
 
-    map.on('pm:create', e => {
-        layerGroup.addLayer(e.layer);
-    });
+    geomanSetLayerGroup(map, layerGroup);
 
     let downloadData = document.getElementById("download-data");
     downloadData.addEventListener('click', e => {
         saveToJSONToFile(layerGroup.toGeoJSON(), 'data.geojson');
+    });
+
+    let uploadData = document.getElementById("upload-data");
+    uploadData.addEventListener('change', e => {
+        loadJSONFromFile(e);
+    });
+
+    let clearData = document.getElementById('clear-data');
+    clearData.addEventListener('click', e => {
+        layerGroup.clearLayers();
     });
 
     map.pm.addControls({ position: 'topleft' });
