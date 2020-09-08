4731c7150edaef54909f2caa334e60b92f60b779
Dominik Lameter <dominik@lameter.com>
2020-09-08 16:07:52 -0500

    Added barebones code for image notes controls class.

---
+++ image-notes-controls.js
@@ -0,0 +1,11 @@
+L.Control.ImageNotes = L.Control.extend({
+    onAdd: function(map) {
+    },
+
+    onRemove: function(map) {
+    },
+});
+
+L.control.imageNotes = function(opts) {
+    return new L.Control.ImageNotes(opts);
+};
