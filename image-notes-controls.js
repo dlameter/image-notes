6402042e6c1843c9dd975feae0b1f0e7df2e731c
Dominik Lameter <dominik@lameter.com>
2020-09-08 16:47:36 -0500

    Added a test button to inspect the internals of a Control object.

--- image-notes-controls.js
+++ image-notes-controls.js
@@ -1,5 +1,31 @@
 L.Control.ImageNotes = L.Control.extend({
     onAdd: function(map) {
+        let prefix = 'image-notes';
+        let container = L.DomUtil.create('div', prefix + '-container');
+        let clearButton = this._createButton('Clear', 'clear-button', prefix + '-clear-button', container, this._clearData);
+
+        return container;
+    },
+
+    _createButton: function(html, title, className, container, func) {
+        let button = L.DomUtil.create('a', className, container);
+        button.innerHTML = html;
+        button.href = '#';
+        button.title = title;
+
+        L.DomEvent.disableClickPropagation(button);
+        L.DomEvent.on(button, 'click', L.DomEvent.stop);
+        L.DomEvent.on(button, 'click', func, this);
+        L.DomEvent.on(button, 'click', this._refocusOnMap, this);
+
+        return button;
+    },
+
+    _createInput: function() {
+    },
+
+    _clearData: function(e) {
+        console.log(this);
     },
 
     onRemove: function(map) {
