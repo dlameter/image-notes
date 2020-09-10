7fb9f8dbc7b7c57b96bda9e1826e608d03f645a4
Dominik Lameter <dominik@lameter.com>
2020-09-10 16:45:45 -0500

    Refactored code while adding functionality to format the elements using css.

--- image-notes-controls.js
+++ image-notes-controls.js
@@ -25,21 +25,22 @@ L.Control.ImageNotes = L.Control.extend({
     onAdd: function(map) {
         let prefix = this.options.prefix;
 
-        let container = L.DomUtil.create('div', prefix + '-container');
-        let clearButton = this._createButton('Clear', 'clear-button', prefix + '-clear-button', container, this._clearData);
-        let saveButton = this._createButton('Save', 'save-button', prefix + '-save-button', container, this._saveData);
-        let loadImage = this._createFileInput('Load Image', 'load-image', '.png, .jpeg, .jpg', container, this.options.loadImageFile);
-        let loadData = this._createFileInput('Data Image', 'load-data', '.json, .geojson', container, this.options.loadDataFile);
+        let container = L.DomUtil.create('div', 'leaflet-bar leaflet-control ' + prefix + '-container');
+
+        let saveButton = this._createButton('Save', prefix + '-save-button', prefix + '-save-icon', container, this._saveData);
+        let loadImage = this._createFileInput('Load Image', '.png, .jpeg, .jpg', prefix + '-upload-image-icon', container, this.options.loadImageFile);
+        let loadData = this._createFileInput('Load Data', '.json, .geojson', prefix + '-upload-json-icon', container, this.options.loadDataFile);
+        let clearButton = this._createButton('Clear', prefix + '-clear-button', prefix + '-clear-icon', container, this._clearData);
 
         return container;
     },
 
-    _createFileInput: function(text, name, accept, container, loadFiles) {
+    _createFileInput: function(text, accept, iconClass, container, loadFiles) {
         let prefix = this.options.prefix;
 
-        let fileDiv = L.DomUtil.create('div', prefix + '-input-wrapper', container);
+        let fileDiv = L.DomUtil.create('div', prefix + '-button-wrapper', container);
 
-        let fileInput = this._createFileInputElement(name, accept, prefix + '-input', fileDiv, (e) => {
+        let fileInput = this._createFileInputElement(text, accept, prefix + '-input', fileDiv, (e) => {
             fileInput.value = '';
         }, (e) => {
             if (fileInput.value) {
@@ -47,12 +48,19 @@ L.Control.ImageNotes = L.Control.extend({
             }
         });
         
-        let fileButton = this._createButton(text, name + '-button', prefix + '-button', fileDiv, (e) => fileInput.click());
+        let fileButton = this._createButtonElement(text, prefix + '-button', iconClass, fileDiv, (e) => fileInput.click());
     },
 
-    _createButton: function(html, title, className, container, func) {
-        let button = L.DomUtil.create('a', className, container);
-        button.innerHTML = html;
+    _createButton: function(title, className, iconClass, container, func) {
+        let buttonContainer = L.DomUtil.create('div', 'image-notes-button-wrapper', container);
+
+        let button = this._createButtonElement(title, className, iconClass, buttonContainer, func);
+
+        return buttonContainer;
+    },
+
+    _createButtonElement: function(title, className, iconClass, container, func) {
+        let button = L.DomUtil.create('a', 'image-notes-icon-wrapper ' + className, container);
         button.href = '#';
         button.title = title;
 
@@ -61,14 +69,15 @@ L.Control.ImageNotes = L.Control.extend({
         L.DomEvent.on(button, 'click', func, this);
         L.DomEvent.on(button, 'click', this._refocusOnMap, this);
 
+        let icon = L.DomUtil.create('div', 'image-notes-icon control-icon ' + iconClass, button);
+
         return button;
     },
 
-    _createFileInputElement: function(name, accept, className, container, clickFunc, changeFunc) {
+    _createFileInputElement: function(text, accept, className, container, clickFunc, changeFunc) {
         let input = L.DomUtil.create('input', className, container);
         input.accept = accept;
-        input.id = name;
-        input.name = name;
+        input.name = text;
         input.style.display = 'none';
         input.type = 'file';
 
